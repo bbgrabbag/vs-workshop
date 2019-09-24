5a6d9f0c0924a815b96c7c9dce400cdef7e9493b
Ben Turner <bbgrabbag@gmail.com>
2019-09-23 23:16:53 -0600

    DOM elements assigned ID's and classes, and JS bindings initialized

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-/** @file Root JS file */
+/** @file Root JS file. Handles intializing element bindings and event listeners */
 
 import './assets/styles.css';
+
+window.addEventListener('load', () => {
+    const calculator = document.getElementById('controls') as HTMLFormElement;
+    const buttons = document.querySelectorAll('.input-btn') as NodeList;
+    const output = document.getElementById('output') as HTMLSpanElement;
+
+});
